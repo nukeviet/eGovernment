4ce59c59d8142a6487342cddae6b63a59d610f40
Vu Thao <vuthao27@gmail.com>
2015-11-09 14:04:34 +0700

    Sửa lỗi xác định sai thư mục

--- modules/organs/funcs/main.php
+++ modules/organs/funcs/main.php
@@ -26,15 +26,15 @@ foreach ( $global_organ_rows as $organid => $organinfo )
 		}
 
         $person_data = array();
-        $sql = "SELECT t1.* FROM " . NV_PREFIXLANG . "_" . $module_data . "_person t1 inner join " . NV_PREFIXLANG . "_" . $module_data . "_rows t2 on t1.organid = t2.organid WHERE t1.organid IN (" . $lid . ") AND t1.active=1 ORDER BY t2.orders,t1.weight";
+        $sql = 'SELECT t1.* FROM ' . NV_PREFIXLANG . '_' . $module_data . '_person t1 inner join ' . NV_PREFIXLANG . '_' . $module_data . '_rows t2 on t1.organid = t2.organid WHERE t1.organid IN (' . $lid . ') AND t1.active=1 ORDER BY t2.orders,t1.weight';
         $result = $db->query( $sql );
         while ( $row = $result->fetch() )
         {
-        	if( !empty( $row['photo'] ) and file_exists( NV_ROOTDIR . NV_BASE_SITEURL . NV_ASSETS_DIR . '/' . $module_upload . '/' . $row['photo'] ) )
+        	if( !empty( $row['photo'] ) and file_exists( NV_ROOTDIR . '/' . NV_FILES_DIR . '/' . $module_upload . '/' . $row['photo'] ) )
 			{
-				$row['photo'] = NV_BASE_SITEURL . NV_ASSETS_DIR . '/' . $module_upload . '/' . $row['photo'];
+				$row['photo'] = NV_BASE_SITEURL . NV_FILES_DIR . '/' . $module_upload . '/' . $row['photo'];
 			}
-			elseif( !empty( $row['photo'] ) and file_exists( NV_ROOTDIR . NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $row['photo'] ) )
+			elseif( !empty( $row['photo'] ) and file_exists( NV_ROOTDIR . '/' . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $row['photo'] ) )
 			{
 				$row['photo'] = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $row['photo'];
 			}
@@ -43,11 +43,11 @@ foreach ( $global_organ_rows as $organid => $organinfo )
 				$row['photo'] = NV_BASE_SITEURL . 'themes/' . $global_config['site_theme'] . '/images/' . $module_file . '/no-avatar.jpg';
 			}
 
-            $row['link'] = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=person/" . $global_organ_rows[$organid]['alias'] . "-" . $organid . "/" . change_alias( $row['name'] ) . "-" . $row['personid'];
+            $row['link'] = NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&' . NV_OP_VARIABLE . '=person/' . $global_organ_rows[$organid]['alias'] . '-' . $organid . '/' . change_alias( $row['name'] ) . '-' . $row['personid'];
             $person_data[] = $row;
         }
         $array_content[] = array(
-            "id" => $organinfo['organid'], "suborgan" => $organinfo['suborgan'], "data" => $person_data
+            'id' => $organinfo['organid'], 'suborgan' => $organinfo['suborgan'], 'data' => $person_data
         );
         unset( $person_data );
     }
